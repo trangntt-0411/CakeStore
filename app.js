eae069380c33326483f6035bae742899975a2b68
17021073 <43052759+17021073@users.noreply.github.com>
2019-11-15 06:45:27 +0700

    add page_product

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ cart.addEventListener('click',()=>{
     }
     
 });
+
+//tabs
+var tab_title = document.getElementsByClassName('tablink');
+var tab_content = document.getElementsByClassName('tf_content');
+function showContent(id) {
+    for(var i = 0; i<tab_content.length; i++) {
+        tab_content[i].style.display = 'none';
+    }
+    var content = document.getElementById(id);
+    content.style.display = 'block';
+}
+
+var ttkhac = document.getElementById('thongtinkhac');
+var ttsp = document.getElementById('thongtinsp');
+showContent(ttsp.textContent);
+ttkhac.addEventListener('click',()=>{
+    ttkhac.className += " active";
+    ttsp.classList.remove('active');
+    var id = ttkhac.textContent;
+    showContent(id);
+});
+ttsp.addEventListener('click',()=>{
+    ttsp.className += " active";
+    ttkhac.classList.remove('active');
+    var id = ttsp.textContent;
+    showContent(id);
+});
